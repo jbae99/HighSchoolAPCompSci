080e4c81dc47e9fd5acdf45dc8bc9e4c609ac8ae
jbae99 <jbae99@gmail.com>
2016-09-06 09:08:47 -0700

    Added all of Lesson 2

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,28 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a1 = 26;
+		int a2 = 87;
+		String a3 = "John Bae ";
+		int a4 = 12735 ;
+		String a5 = " Via Terceto ";
+		String a6 = "San Diego, ";
+		String a7 = "California ";
+		int a8 = 92130;
+		int h = 5;
+		int l = 7;
+		int w = 13;
+		int k = 2;
+		String a9 = "The surface area of your rectangle is ";
+	
+		
+		System.out.println(a1 * a2);
+		
+		System.out.println(a3);
+		System.out.println(a4 + a5 + a6 + a7);
+		System.out.println(a8);
+		
+		System.out.println(a9 + k*(h*l + w*l + h*w));
+	}
+}
---
+++ APLesson_02/Lesson_02.java
@@ -0,0 +1,8 @@
+public class Lesson_02
+{
+	public static void main(String[]args)
+	{
+		String var1 = "Skkrrttt";
+		System.out.println(var1);
+	}
+}
