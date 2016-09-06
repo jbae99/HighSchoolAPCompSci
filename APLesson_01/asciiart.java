9b7624f726f4cb2810d1541eb0e9c29f89d419d7
jbae99 <jbae99@gmail.com>
2016-09-06 08:02:00 -0700

    Added files

---
+++ APLesson_01/asciiart.java
@@ -0,0 +1,9 @@
+public class asciiart
+{
+public static void main(String[]args)
+{
+System.out.println("                    (\\___/)                             ");
+System.out.println("                    (='.'=)                             ");
+System.out.println("                    (\")_(\")                             ");
+}
+}
---
+++ APLesson_01/firstprog.java
@@ -0,0 +1,13 @@
+public class firstprog
+{
+	public static void main(String[]args)
+	{
+		System.out.println("                                    ___     ___  ");
+		System.out.println("                                   /   \\~~~/   \\ ");
+		System.out.println("                             ,----(      . .    )");
+		System.out.println("                            /      \\___     ___/  ");
+		System.out.println("                           /|         (\\    |(        ");
+		System.out.println("                          ^ \\   /___\\  /\\   |        ");
+		System.out.println("                             |_|    |_|   \"\"              ");
+	}
+}
