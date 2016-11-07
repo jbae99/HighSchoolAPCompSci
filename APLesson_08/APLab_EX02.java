7d270cce8e39eec577047a600c261efa649ccd32
jbae99 <jbae99@gmail.com>
2016-11-07 09:13:29 -0800

    began lesson 8

---
+++ APLesson_08/APLab_EX02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class APLab_EX02
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.println("Please enter the first word:");
+		String word1 = kb.nextLine();
+		System.out.println("Please enter the second word:");
+		String word2 = kb.nextLine();
+		System.out.println("Please enter the third word:");
+		String word3 = kb.nextLine();
+		
+		
+	}
+}
